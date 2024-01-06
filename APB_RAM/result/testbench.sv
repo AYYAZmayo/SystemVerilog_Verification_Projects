6d2fcfaf7b79876f6f1d6d765a7c4280ff6b973a
AYYAZmayo <ayyazmayo@gmail.com>
2024-01-06 19:12:29 +0500

    Added new designs and TB

---
+++ APB_RAM/result/testbench.sv
@@ -0,0 +1,382 @@
+interface apb_if;
+  
+  logic presetn;
+  logic pclk;
+  logic psel;
+  logic penable;
+  logic pwrite;
+  logic [31:0] paddr, pwdata;
+  logic [31:0] prdata;
+  logic pready, pslverr;  
+  
+  
+endinterface
+
+class transaction;
+	typedef enum int {write =0, read = 1, random = 2, error=3} opr_type;
+	randc opr_type operation;
+	rand bit psel;
+	rand bit penable;
+	rand bit pwrite;
+	rand bit [31:0] paddr, pwdata;
+	bit [31:0] prdata;
+	bit pready;
+	bit pslverr;
+	
+	 constraint addr_c {
+  paddr > 1; paddr < 32;////2 3 4
+  }
+  
+  constraint data_c {
+  pwdata > 1; pwdata < 5000; /// 2-9
+  }
+	function void display(input string tag);
+		$display("[%0s] : op=%0s , paddr =%0d , pwdata =%0d, psel=%0d, penable =%0d, pwrite =%0d, prdata=%0d, pready=%0d, pslverr =%0d",tag,
+					operation.name(), paddr, pwdata, psel, penable, pwrite, prdata, pready, pslverr);
+	endfunction
+	
+	function transaction copy();
+			copy=new();
+			copy.operation =this.operation;
+			copy.psel = this.psel;
+			copy.penable = this.penable;
+			copy.pwrite = this.pwrite;
+			copy.paddr = this.paddr;
+			copy.pwdata = this.pwdata;
+			copy.prdata = this.prdata;
+			copy.pslverr =this. pslverr;
+	endfunction
+	
+	endclass
+	
+	class generator;
+		transaction tr;
+		mailbox #(transaction) mbxdr;
+		
+		event done;
+		event nextdr;
+		event nextscb;
+		int count=0;
+		
+		function new(mailbox #(transaction) mbxdr);
+			this.mbxdr = mbxdr;
+			tr =new();
+		endfunction
+		
+		task run();
+			repeat (count) begin
+				assert(tr.randomize()) else $display("Randomization is failed");
+				mbxdr.put(tr.copy);
+				tr.display("GEN");
+				@(nextdr);
+				@(nextscb);
+			end
+			
+			->done;
+		endtask
+		
+	endclass
+	
+	class driver;
+		transaction tr;
+		mailbox #(transaction) mbxdr;
+		virtual apb_if vif;
+		event nextdr;
+		
+		
+		function new(mailbox #(transaction) mbxdr);
+			this.mbxdr =mbxdr;
+		endfunction
+		
+		
+		task reset ();
+			vif.presetn<=0;
+			vif.psel <=0;
+			vif.penable <=0;
+			vif.pwrite <= 0;
+			vif.paddr <=0;
+			vif.pwdata <=0;
+			repeat (5) @(posedge vif.pclk);
+			vif.presetn <=1;
+			repeat (5) @(posedge vif.pclk);
+			$display("---------------------------------");
+			$display("RESET is DONE");
+			$display("---------------------------------");
+		endtask
+		
+		task write();
+			@(posedge vif.pclk);
+			vif.psel<= 1'b1;
+			vif.pwrite <=1'b1;
+			vif.penable <=1'b0;
+			vif.paddr <= tr.paddr;
+			vif.pwdata <= tr.pwdata;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b1;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b0;
+			vif.psel<= 1'b0;
+			vif.pwrite <=1'b0;
+			$display("[DRV] : DATA WRITE OP data :%0d and addr : %0d", tr.pwdata, tr.paddr);
+		endtask
+		
+		task read();
+			@(posedge vif.pclk);
+			vif.psel<= 1'b1;
+			vif.penable <=1'b0;
+			vif.pwrite <=1'b0;
+			//vif.pwdata <= tr.pwdata;
+			vif.paddr <= tr.paddr;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b1;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b0;
+			vif.psel<= 1'b0;
+			vif.pwrite <=1'b0;
+			$display("[DRV] : DATA READ OP addr : %0d", tr.paddr);
+			
+		endtask
+		
+		task random();
+			@(posedge vif.pclk);
+			vif.psel<= 1;
+			vif.penable <=0;
+			//random values
+			vif.pwrite <=tr.pwrite;
+			vif.paddr <= tr.paddr;
+			vif.pwdata <= tr.pwdata;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b0;
+			vif.psel<= 1'b0;
+			vif.pwrite <=1'b0;
+			$display("[DRV] : RANDOM OPERATION");
+		endtask
+		
+		task error();
+			@(posedge vif.pclk);
+			vif.psel<= 1'b1;
+			vif.penable <=1'b0;
+			//SLAVE error generation condition
+			vif.pwrite <=tr.pwrite;
+			vif.paddr <= $urandom_range(33,100); // should be beyond the range 
+			vif.pwdata <= tr.pwdata;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b1;
+			repeat(1) @(posedge vif.pclk);
+			vif.penable <=1'b0;
+			vif.psel<= 1'b0;
+			vif.pwrite <=1'b0;
+			$display("[DRV] : SLV ERROR");
+		endtask
+		
+		task run();
+			forever begin
+				mbxdr.get(tr);
+				if (tr.operation == 0)begin
+					write();
+				end
+				else if (tr.operation == 1)begin
+					read();
+				end
+				else if (tr.operation == 2)begin
+					random();
+				end
+				else if (tr.operation == 3)begin
+					error();
+				end
+				->nextdr;
+			end
+		endtask
+	endclass
+	//////////////////////////////////////////////////////////
+	class monitor;
+		virtual apb_if vif;
+		transaction tr;
+		mailbox #(transaction) mbxms;
+		
+		function new(mailbox #(transaction) mbxms);
+			this.mbxms=mbxms;
+			tr =new();
+		endfunction
+		
+		task run();
+			forever begin
+				@(posedge vif.pclk);
+				// detect the starting condition of a transaction 
+				if (vif.psel == 1 && vif.penable != 1)begin
+					// write acess check
+					@(posedge vif.pclk);
+					if (vif.psel == 1 && vif.penable == 1 && vif.pwrite==1) 
+					begin
+						@(posedge vif.pclk);
+						tr.pwrite = vif.pwrite;
+						tr.paddr = vif.paddr;
+						tr.pwdata =vif.pwdata;
+						tr.pready =vif.pready;
+						tr.pslverr = vif.pslverr;
+						$display("[MON] : DATA WRITE data : %0d and addr : %0d write :%0b", vif.pwdata, vif.paddr, vif.pwrite);
+                       //@(posedge vif.pclk);
+					end
+					// read acess check
+					//@(posedge vif.pclk);
+					else if (vif.psel == 1 && vif.penable == 1 && vif.pwrite==0)
+					begin
+						@(posedge vif.pclk);
+						tr.pwrite = vif.pwrite;
+						tr.paddr = vif.paddr;
+						tr.pwdata =vif.pwdata;
+						tr.prdata= vif.prdata;
+						tr.pready =vif.pready;
+						tr.pslverr = vif.pslverr;
+						@(posedge vif.pclk);
+						$display("[MON] : DATA READ data : %0d and addr : %0d write:%0b", vif.prdata,vif.paddr, vif.pwrite);
+						//@(posedge vif.pclk);
+					end
+					mbxms.put(tr);
+					@(posedge vif.pclk);
+				end
+			end
+		endtask
+	endclass
+	/////////////////////////////////////////
+	class scoreboard;
+		transaction tr_mon;
+		mailbox #(transaction) mbxms;
+		
+		bit [31:0]temp_wdata[31:0]='{default:0};
+		bit [31:0]rdata;
+		
+		event nextscb;
+		
+		function new(mailbox #(transaction) mbxms);
+			this.mbxms = mbxms;
+		endfunction
+		
+		task run();
+			forever begin
+				mbxms.get(tr_mon);
+				$display("[SCO] : DATA RCVD wdata:%0d rdata:%0d addr:%0d write:%0b", tr_mon.pwdata, tr_mon.prdata, tr_mon.paddr, tr_mon.pwrite);
+				if (tr_mon.pwrite==1 && tr_mon.pslverr==0)
+				begin
+					temp_wdata[tr_mon.paddr] =tr_mon.pwdata;
+					$display("[SCO] : DATA STORED DATA : %0d ADDR: %0d",tr_mon.pwdata, tr_mon.paddr);
+				end
+				
+				else if (tr_mon.pwrite==0 && tr_mon.pslverr==0)
+				begin
+					rdata = temp_wdata[tr_mon.paddr];
+                  if( tr_mon.prdata == rdata)begin
+						$display("[SCO] : Data Matched"); 
+                  		$display("---------------------------------");
+                  end
+					else begin
+						$display("[SCO] : Data Mismatched"); 
+                  		$display("---------------------------------");
+                    end
+				end
+				
+				else if (tr_mon.pslverr)
+				begin
+					$display("[SCO] : SLV ERROR DETECTED");
+                  	$display("---------------------------------");
+				end
+				->nextscb;
+			end
+		endtask
+	endclass
+	/////////////////////////////////////////
+	class environment;
+		generator gen;
+		driver drv;
+		monitor mon;
+		scoreboard scb;
+		
+		mailbox #(transaction) mbxdr;
+		mailbox #(transaction) mbxms;
+		
+		event nextgd;
+		event nextgs;
+		
+		virtual apb_if vif;
+		
+		function new (virtual apb_if vif);
+			mbxdr =new();
+			mbxms = new();
+			
+			gen =new(mbxdr);
+			drv = new(mbxdr);
+			
+			mon =new(mbxms);
+			scb =new(mbxms);
+			
+			this.vif =vif;
+			drv.vif =this.vif;
+			mon.vif =this.vif;
+			
+			gen.nextdr =nextgd;
+			drv.nextdr =nextgd;
+			
+			gen.nextscb =nextgs;
+			scb.nextscb =nextgs;
+		endfunction
+		
+		task pre_test();
+			drv.reset();
+		endtask
+		
+		task test();
+			fork
+				gen.run();
+				drv.run();
+				mon.run();
+				scb.run();
+			join_any
+		
+		endtask
+		
+		task post_test();
+			wait(gen.done.triggered);
+			$finish();
+		endtask
+		
+		task run();
+			pre_test();
+			test();
+			post_test();
+		endtask
+	
+	endclass
+	
+module tb_top;
+	
+	   apb_if vif();
+ 
+		apb_ram dut (vif.presetn, vif.pclk, vif.psel, vif.penable, vif.pwrite, vif.paddr, vif.pwdata, vif.prdata, vif.pready,vif.pslverr);
+   
+    initial begin
+      vif.pclk <= 0;
+    end
+    
+    always #10 vif.pclk <= ~vif.pclk;
+    
+    environment env;
+    
+    
+    
+    initial begin
+      env = new(vif);
+      env.gen.count = 5;
+      env.run();
+    end
+      
+    
+    initial begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    end
+   
+    
+ endmodule
+	
---
+++ FIFO/my_fifo_tb.sv
@@ -0,0 +1,316 @@
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
+//////////////////////////////////////
+ 
+// Define an interface for the FIFO
+interface fifo_if;
+  
+  logic clk, rd, wr;         // Clock, read, and write signals
+  logic full, empty;           // Flags indicating FIFO status
+  logic [7:0] data_in;         // Data input
+  logic [7:0] data_out;        // Data output
+  logic rst;                   // Reset signal
+ 
+endinterface
+
+///////////////////////////////////////////////
+class transaction;
+bit rd,wr;
+bit full,empty;
+bit [7:0] data_in;
+bit [7:0] data_out;
+rand bit oper;
+
+constraint ctrl {
+		oper dist {1:/50 , 0:/50};
+}
+
+endclass
+
+//////////////////////////////////////////////
+
+class generator;
+transaction tr;
+mailbox #(transaction) mbx;
+int count=0;
+int i=0; 
+event next;
+event done;
+
+function new(mailbox #(transaction) mbx);
+
+	this.mbx=mbx;
+	tr=new();
+	endfunction
+task run();
+	repeat (count) begin
+		assert (tr.randomize) else $display("Randomization is failed");
+		mbx.put(tr);
+		i++;
+		$display("[GEN]: Operation : %0d , and iteration :%0d",tr.oper,i);
+		@(next);
+	end
+	->done;
+	endtask
+endclass
+
+//////////////////////////////////////////////////
+
+class driver;
+transaction td;
+mailbox #(transaction) mbx;
+virtual fifo_if itf;
+
+function new(mailbox #(transaction) mbx);
+	this.mbx=mbx;
+endfunction
+
+task reset();
+		itf.rst <= 1'b1;
+		itf.wr <= 1'b0;
+		itf.rd <= 1'b0;
+		itf.data_in <= 'b0;
+		repeat (5) @(posedge itf.clk);
+		itf.rst<= 1'b0;
+    $display("[DRV] : DUT Reset Done");
+    $display("------------------------------------------");
+	endtask
+task write();
+		@(posedge itf.clk);
+		itf.rst <=1'b0;
+		itf.rd  <=1'b0;
+		itf.wr  <=1'b1;
+		itf.data_in <= $urandom_range(1,10);
+		@(posedge itf.clk);
+		itf.wr<=1'b0;
+  $display("[DRV] : DATA WRITE  data : %0d", itf.data_in);
+		@(posedge itf.clk);
+	endtask
+task read();
+		@(posedge itf.clk);
+		itf.rst <=1'b0;
+		itf.rd  <=1'b1;
+		@(posedge itf.clk);
+		itf.rd<=1'b0;
+		$display("[DRV] : DATA READ");
+		@(posedge itf.clk);
+	endtask
+task write_till_full();
+		for (int i=0;i<16;i++) begin
+		@(posedge itf.clk);
+		itf.rst <=1'b0;
+		itf.rd  <=1'b0;
+		itf.wr  <=1'b1;
+		itf.data_in <= $urandom_range(1,200);
+		@(posedge itf.clk);
+		itf.wr<=1'b0;
+          $display("[DRV] : DATA WRITE  data : %0d in FIFO full task", itf.data_in);
+		@(posedge itf.clk);
+		end
+	endtask
+	
+	task read_till_empty();
+	for (int i=0; i<16 ;i++)begin
+		@(posedge itf.clk);
+		itf.rst <=1'b0;
+		itf.rd  <=1'b1;
+		@(posedge itf.clk);
+		itf.rd<=1'b0;
+      $display("[DRV] : DATA READ FIFO empty task");
+		@(posedge itf.clk);
+		end
+	endtask
+	
+	
+task run();
+	forever begin
+		mbx.get(td);
+      	write_till_full();
+		read_till_empty();
+		if (td.oper == 1)
+			write();
+		else
+			read();
+
+		end
+	endtask
+endclass
+//////////////////////////////////////////////////////////	
+
+class monitor;
+
+virtual fifo_if itf;
+transaction tm;
+mailbox #(transaction) mbx;
+
+function new(mailbox #(transaction) mbx);
+
+	this.mbx = mbx;
+	endfunction
+task run();
+	tm =new();
+	forever begin
+      repeat (2) @(posedge itf.clk);
+	tm.rd  = itf.rd;
+	tm.wr  = itf.wr;
+	tm.full = itf.full;
+	tm.empty = itf.empty;
+	tm.data_in = itf.data_in;
+      @(posedge itf.clk);
+	tm.data_out = itf.data_out;
+	mbx.put(tm);
+      $display("[MON] : Wr:%0d rd:%0d din:%0d dout:%0d full:%0d empty:%0d", tm.wr, tm.rd, tm.data_in, tm.data_out, tm.full, tm.empty);
+	end
+	endtask
+endclass
+//////////////////////////////////////
+/////////////////////////////////////////////////////
+ 
+class scoreboard;
+  
+  mailbox #(transaction) mbx;  // Mailbox for communication
+  transaction tr;          // Transaction object for monitoring
+  event next;
+  bit [7:0] din[$];       // Array to store written data
+  bit [7:0] temp;         // Temporary data storage
+  int err = 0;            // Error count
+  
+  function new(mailbox #(transaction) mbx);
+    this.mbx = mbx;     
+  endfunction;
+ 
+  task run();
+    forever begin
+      mbx.get(tr);
+      $display("[SCO] : Wr:%0d rd:%0d din:%0d dout:%0d full:%0d empty:%0d", tr.wr, tr.rd, tr.data_in, tr.data_out, tr.full, tr.empty);
+      
+      if (tr.wr == 1'b1) begin
+        if (tr.full == 1'b0) begin
+          din.push_front(tr.data_in);
+          $display("[SCO] : DATA STORED IN QUEUE :%0d", tr.data_in);
+        end
+        else begin
+          $display("[SCO] : FIFO is full");
+        end
+        $display("--------------------------------------"); 
+      end
+    
+      if (tr.rd == 1'b1) begin
+        if (tr.empty == 1'b0) begin  
+          temp = din.pop_back();
+          
+          if (tr.data_out == temp)
+            $display("[SCO] : DATA MATCH");
+          else begin
+            $error("[SCO] : DATA MISMATCH");
+            err++;
+          end
+        end
+        else begin
+          $display("[SCO] : FIFO IS EMPTY");
+        end
+        
+        $display("--------------------------------------"); 
+      end
+      
+      -> next;
+    end
+  endtask
+  
+endclass
+///////////////////////////////////////////////////////////////////
+
+class environment;
+
+generator gen;
+driver drv;
+monitor mon;
+scoreboard scb;
+
+mailbox #(transaction) gdmbx;
+mailbox #(transaction) msmbx;
+
+event nexttgs;
+virtual fifo_if itf;
+  
+function new (virtual fifo_if itf);
+
+gdmbx = new();
+gen = new(gdmbx);
+drv = new(gdmbx);
+
+msmbx = new();
+mon = new(msmbx);
+scb = new(msmbx);
+this.itf = itf;
+
+  
+  // driver interface connect
+  drv.itf = this.itf;
+  //monitor interace initialize
+  mon.itf = this.itf;
+
+gen.next=nexttgs;
+scb.next=nexttgs;
+
+endfunction
+
+task pre_test();
+  drv.reset();
+	endtask
+	
+task test();
+	fork 
+	gen.run();
+	drv.run();
+	mon.run();
+	scb.run();
+    join_any  /////////
+endtask
+
+task post_test();
+  wait (gen.done.triggered);
+	$display("---------------------------------------------");
+  $display("Error Count :%0d", scb.err);
+    $display("---------------------------------------------");
+    $finish();
+	endtask
+task run();
+  pre_test();
+  test();
+  post_test();
+endtask
+
+endclass
+////////////////////////////////////////////////////
+
+module tb;
+
+fifo_if itf();
+
+initial begin
+	itf.clk=0;
+end	
+
+always #10 itf.clk<=~itf.clk;
+
+
+FIFO dut (.clk(itf.clk), .rst(itf.rst), .wr(itf.wr), .rd(itf.rd), .din(itf.data_in),.dout(itf.data_out),.empty(itf.empty),.full(itf.full));
+
+  environment env;
+    
+  initial begin
+    env = new(itf);
+    env.gen.count = 50;
+    env.run();
+  end
+    
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+
+
+endmodule
